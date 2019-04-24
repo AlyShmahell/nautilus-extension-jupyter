a0788e2a30dc745da7394b3f8ffc557c43b65d59
Aly Shmahell <aly.shmahell@gmail.com>
2019-04-25 00:48:43 +0200

    renamed requirements installer

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ with open('LICENSE', 'r') as legal:
 class customInstallClass(install):
 	def run(self):
 		install.run(self)
-		os.system("chmod +x ./install.sh")
-		os.system("sh install.sh")
+		os.system("chmod +x ./requirements.sh")
+		os.system("sh requirements.sh")
 	
 
 setup(
